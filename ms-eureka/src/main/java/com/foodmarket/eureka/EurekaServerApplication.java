0acdb3a8df93915312f6d09efb25613eb86b3e53
fssepul-prog <felipeandressepul2025@gmail.com>
2026-05-12 21:34:49 -0400

    Add files via upload

---
+++ ms-eureka/src/main/java/com/foodmarket/eureka/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package com.foodmarket.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Todos los microservicios se registran aqui al iniciar.
+ * Verificar en: http://localhost:8761
+ * Permite que los servicios se encuentren entre si
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
