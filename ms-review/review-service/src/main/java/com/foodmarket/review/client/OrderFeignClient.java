ccccb002dfdd559c8b36fe92efba831d30ed5449
daniel-duoc <da.paradac@duocuc.cl>
2026-05-19 18:13:04 -0400

    Change Feign client name from 'order-service' to 'ms-order'

--- ms-review/review-service/src/main/java/com/foodmarket/review/client/OrderFeignClient.java
+++ ms-review/review-service/src/main/java/com/foodmarket/review/client/OrderFeignClient.java
@@ -1,6 +1,6 @@
 package com.foodmarket.review.client;
 import lombok.Data; import org.springframework.cloud.openfeign.FeignClient; import org.springframework.web.bind.annotation.*;
-@FeignClient(name="order-service")
+@FeignClient(name = "ms-order")
 public interface OrderFeignClient {
     @GetMapping("/orders/{id}") OrderStatusDTO getOrder(@PathVariable("id") Long id);
     @Data class OrderStatusDTO { private Long id; private String status; }
