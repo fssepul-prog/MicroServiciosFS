62820447a4e57c1b4b1eee2068ee57e0b0ec3d29
daniel-duoc <da.paradac@duocuc.cl>
2026-05-19 01:42:05 -0400

    Delete ms-notification (duplicado)

--- ms-notification/src/main/java/com/foodmarket/notification/repository/NotificationRepository.java
+++
@@ -1,6 +0,0 @@
-package com.foodmarket.notification.repository;
-import com.foodmarket.notification.model.Notification; import org.springframework.data.jpa.repository.JpaRepository; import java.util.List;
-public interface NotificationRepository extends JpaRepository<Notification,Long> {
-    List<Notification> findByRecipientIdOrderByCreatedAtDesc(Long recipientId);
-    List<Notification> findByRecipientIdAndReadFalse(Long recipientId);
-}
