33e878065461177bdf5637cba9d96dd7010670f2
daniel-duoc <da.paradac@duocuc.cl>
2026-05-19 17:11:10 -0400

    Delete ms-review/src/main directory (duplicado)

--- ms-review/src/main/java/com/foodmarket/review/client/OrderFeignClient.java
+++
@@ -1,7 +0,0 @@
-package com.foodmarket.review.client;
-import lombok.Data; import org.springframework.cloud.openfeign.FeignClient; import org.springframework.web.bind.annotation.*;
-@FeignClient(name="ms-order")
-public interface OrderFeignClient {
-    @GetMapping("/orders/{id}") OrderStatusDTO getOrder(@PathVariable("id") Long id);
-    @Data class OrderStatusDTO { private Long id; private String status; }
-}
--- ms-review/src/main/java/com/foodmarket/review/exception/ResourceNotFoundException.java
+++
@@ -1,2 +0,0 @@
-package com.foodmarket.review.exception;
-public class ResourceNotFoundException extends RuntimeException { public ResourceNotFoundException(String m){super(m);} }
