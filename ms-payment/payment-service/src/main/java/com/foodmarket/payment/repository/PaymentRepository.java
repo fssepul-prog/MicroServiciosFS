785549747d4c2e6537aa028b975031f0d9d36b56
daniel-duoc <da.paradac@duocuc.cl>
2026-05-19 18:17:41 -0400

    Delete ms-payment directory

--- ms-payment/payment-service/src/main/java/com/foodmarket/payment/repository/PaymentRepository.java
+++
@@ -1,6 +0,0 @@
-package com.foodmarket.payment.repository;
-import com.foodmarket.payment.model.Payment; import org.springframework.data.jpa.repository.JpaRepository; import java.util.*;
-public interface PaymentRepository extends JpaRepository<Payment,Long> {
-    Optional<Payment> findByOrderId(Long orderId);
-    List<Payment> findByCustomerIdOrderByCreatedAtDesc(Long customerId);
-}
--- ms-payment/src/main/java/com/foodmarket/payment/exception/ResourceNotFoundException.java
+++
@@ -1,2 +0,0 @@
-package com.foodmarket.payment.exception;
-public class ResourceNotFoundException extends RuntimeException { public ResourceNotFoundException(String m){super(m);} }
